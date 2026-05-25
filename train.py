132247bb02be415d57f6ed2b9bb7ef9ef2486099
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 05:20:39 +0000

    Auto-format by https://ultralytics.com/actions

--- train.py
+++ train.py
@@ -1,23 +1,24 @@
-from ultralytics import YOLO
 import multiprocessing as mp
+
+from ultralytics import YOLO
 
 
 def main():
 
     model = YOLO(r"ultralytics/cfg/models/dream/DREAM.yaml")
 
-    train_results = model.train(
+    model.train(
         data=r"R:\Dream\dataset\visdrone2021\visdrone-yolo11.yaml",  # dataset
         epochs=300,
         imgsz=640,
         device="0",  # 运行设备（例如 'cpu', 0, [0,1,2,3]）
-        project='runs\paper',
-        name='Dream',
+        project="runs\paper",
+        name="Dream",
         exist_ok=True,
-        batch = 8,
-        cfg = "ultralytics/cfg/hyp_visdrone.yaml",
+        batch=8,
+        cfg="ultralytics/cfg/hyp_visdrone.yaml",
         workers=5,
-        resume=False ,
+        resume=False,
     )
 
 
